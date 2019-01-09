a5380d1e3fa20d854c4a821b388f87a3e1391635
Mukul Shakya <muklshakya123@gmail.com>
2019-01-09 08:13:39 +0530

    Fixed projects page

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ app.get('/about', (request, response) => {
     });
 });
 
-app.get('projects', (request, response) => {
-    response.render('projects.hbs', {
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs',{
         pageTitle: "Projects Page"
-    })
+    });
 });
 
 app.get('/bad', (request, response) => {
