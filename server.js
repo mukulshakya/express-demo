3d5f34b731a8446aee8cb55af4a3357fea7fb8fa
Mukul Shakya <muklshakya123@gmail.com>
2019-01-09 08:03:51 +0530

    Added projects page

--- server.js
+++ server.js
@@ -57,6 +57,12 @@ app.get('/about', (request, response) => {
     });
 });
 
+app.get('projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: "Projects Page"
+    })
+});
+
 app.get('/bad', (request, response) => {
     response.send({
         errorMessage: "Unable to Handle Request"
